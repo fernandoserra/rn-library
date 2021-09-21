22b40c956792e08f732850c03b9606ef48ccb3c5
Fernando Serra <fernando.serra.r@gmail.com>
2021-09-20 22:42:28 -0300

    react native library

---
+++ rn-library/index.js
@@ -0,0 +1,27 @@
+import React , {Component} from "react";
+import { View, Text,StyleSheet} from "react-native";
+
+class Test extends Component{
+
+    render(){
+        return(
+            <View style={styles.container}>
+                <Text style={styles.text}>Test Library</Text>
+            </View>
+        );
+    }
+}
+
+const styles= StyleSheet.create({
+    container:{
+        flex:1,
+        justifyContent:"center",
+        alignItems:"center"
+    },
+    text:{
+        fontSize:30
+    }
+})
+
+
+export default Test;
